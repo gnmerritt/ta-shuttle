2812bde442d87b8b9c836b8036e5193f2213e8d2
Nathan Merritt <gnm@the-merritts.net>
2013-09-25 18:45:16 -0400

    the form now reverses itself at noon to track homeward commutes

--- shuttle.py
+++ shuttle.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -22,9 +22,51 @@ class Arrival(db.Model):
                                           b=self.destination,
                                           t=self.time)
 
+
+class Endpoint(object):
+    def __init__(self, val, name):
+        self.val = val
+        self.name = name
+        self.default = False
+
+
+CAMBRIDGE = [Endpoint("cambridge", "Cambridge")]
+OFFICES = [
+    Endpoint("newton141", "141 Needham St"),
+    Endpoint("kendrick", "Kendrick Street"),
+]
+MORNING_PICKUPS = [
+    Endpoint("kendall", "Kendall Square"),
+    Endpoint("central", "Central Square"),
+]
+
+def is_inbound():
+    """Returns whether the shuttles are going inbound"""
+    now = datetime.now()
+    return now.hour < 12
+
+def get_stops():
+    if is_inbound():
+        return MORNING_PICKUPS
+    return OFFICES
+
+def get_destinations():
+    if is_inbound():
+        return OFFICES
+    return CAMBRIDGE
+
+def get_questions():
+    stops = get_stops()
+    destinations = get_destinations()
+    return {
+        "stops":stops,
+        "destinations":destinations
+    }
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    context = get_questions()
+    return render_template("index.html", **context)
 
 @app.route("/time", methods=["POST"])
 def time():
@@ -33,8 +75,11 @@ def time():
     arrival = Arrival(stop, destination)
     db.session.add(arrival)
     db.session.commit()
-    return "Thanks! Recorded that the {s} shuttle to {d} picked up at {t}" \
-      .format(t=datetime.utcnow(), d=destination, s=stop)
+    resp = make_response(
+        "Thanks! Recorded that the {s} shuttle to {d} picked up at {t}"
+        .format(t=datetime.utcnow(), d=destination, s=stop)
+    )
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
